1b6c3e31d045b8a89c0713a507d4fd78e6dc8016
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-12-14 11:55:37 +0100

    Initial commit

---
+++ app/src/main/java/com/dam2jms/cajaregistradoravm/screens/CambiosScreen.kt
@@ -0,0 +1,148 @@
+package com.dam2jms.cajaregistradoravm.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.dam2jms.cajaregistradoravm.R
+import com.dam2jms.cajaregistradoravm.states.UiState
+import com.dam2jms.cajaregistradoravm.viewmodels.ViewModelCambios
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun cambiosScreenState(navController: NavController, mvvm: ViewModelCambios) {
+
+    val nonesUiState by mvvm.uiState.collectAsState()
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "CAMBIOS DINERO" )},
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Atras")
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        cambiosScreenContent(modifier = Modifier.padding(paddingValues), mvvm, nonesUiState)
+    }
+}
+
+@Composable
+fun cambiosScreenContent(modifier: Modifier, mvvm: ViewModelCambios, state: UiState) {
+
+    var mostrarAlertDialog by rememberSaveable { mutableStateOf(false) }
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            // Modifico el tamaño de las imágenes
+            listOf(
+                R.drawable.billete_cien,
+                R.drawable.billete_cincuenta,
+                R.drawable.billete_veinte,
+                R.drawable.billete_diez,
+                R.drawable.billete_cinco,
+                R.drawable.moneda_dos,
+                R.drawable.moneda_uno
+            ).forEach { imageResource ->
+                Image(
+                    painter = painterResource(id = imageResource),
+                    contentDescription = null,
+                    modifier = Modifier.size(50.dp)
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            // Aseguro que las claves existan en el mapa antes de acceder
+            listOf(100, 50, 20, 10, 5, 2, 1).forEach { billete ->
+                Text(text = "${state.listaCambios[billete] ?: 0}")
+            }
+        }
+
+        OutlinedTextField(
+            value = state.importe.toString(),
+            onValueChange = { mvvm.onChange(if (it.isEmpty()) 0.0 else it.toDouble(), state.pago) },
+            label = { Text(text = "Importe") },
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        OutlinedTextField(
+            value = state.pago.toString(),
+            onValueChange = {
+                mvvm.onChange(state.importe, if (it.isEmpty()) 0.0 else it.toDouble())
+            },
+            label = { Text(text = "Pago") },
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        
+        Button(
+            onClick = {
+                //llamo al metodo del viewmodel con la logica
+                mvvm.cambios(context = context)
+            }, modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text(text = "DESGLOSAR")
+        }
+    }
+}
---
+++ app/src/main/java/com/dam2jms/cajaregistradoravm/states/UiState.kt
@@ -0,0 +1,10 @@
+package com.dam2jms.cajaregistradoravm.states
+
+import com.dam2jms.cajaregistradoravm.R
+
+data class UiState(
+    var importe: Double = 0.0,
+    var pago: Double = 0.0,
+    var resultado: String = "",
+    var listaCambios: MutableMap<Int, Int> = mutableMapOf()
+)
